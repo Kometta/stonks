2796b995c4f21102bfbb21079f1f56998f0afecd
mantasjjj <mjakaitis18@gmail.com>
2020-09-19 15:59:58 +0300

    Demo version files

---
+++ Stonks/Forms/FormUserData.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Stonks.Forms
+{
+    partial class FormUserData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.iconUser = new FontAwesome.Sharp.IconPictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.labelUserDataName = new System.Windows.Forms.Label();
+            this.labelUserDataLast = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveInfoBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.iconUser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // iconUser
+            // 
+            this.iconUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.iconUser.IconChar = FontAwesome.Sharp.IconChar.User;
+            this.iconUser.IconColor = System.Drawing.Color.White;
+            this.iconUser.IconSize = 84;
+            this.iconUser.Location = new System.Drawing.Point(334, 12);
+            this.iconUser.Name = "iconUser";
+            this.iconUser.Size = new System.Drawing.Size(98, 84);
+            this.iconUser.TabIndex = 0;
+            this.iconUser.TabStop = false;
+            this.iconUser.Click += new System.EventHandler(this.iconUser_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 178);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(489, 178);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(190, 27);
+            this.textBox2.TabIndex = 2;
+            // 
+            // labelUserDataName
+            // 
+            this.labelUserDataName.AutoSize = true;
+            this.labelUserDataName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelUserDataName.ForeColor = System.Drawing.Color.White;
+            this.labelUserDataName.Location = new System.Drawing.Point(104, 155);
+            this.labelUserDataName.Name = "labelUserDataName";
+            this.labelUserDataName.Size = new System.Drawing.Size(92, 20);
+            this.labelUserDataName.TabIndex = 3;
+            this.labelUserDataName.Text = "First Name";
+            // 
+            // labelUserDataLast
+            // 
+            this.labelUserDataLast.AutoSize = true;
+            this.labelUserDataLast.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelUserDataLast.ForeColor = System.Drawing.Color.White;
+            this.labelUserDataLast.Location = new System.Drawing.Point(489, 155);
+            this.labelUserDataLast.Name = "labelUserDataLast";
+            this.labelUserDataLast.Size = new System.Drawing.Size(91, 20);
+            this.labelUserDataLast.TabIndex = 3;
+            this.labelUserDataLast.Text = "Last Name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(104, 287);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(190, 27);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(489, 287);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(190, 27);
+            this.textBox4.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(104, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Date of Birth";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(489, 264);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bank";
+            // 
+            // saveInfoBtn
+            // 
+            this.saveInfoBtn.BackColor = System.Drawing.Color.DarkOrange;
+            this.saveInfoBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.saveInfoBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.saveInfoBtn.FlatAppearance.BorderSize = 0;
+            this.saveInfoBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.saveInfoBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.saveInfoBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.saveInfoBtn.ForeColor = System.Drawing.Color.White;
+            this.saveInfoBtn.Location = new System.Drawing.Point(323, 399);
+            this.saveInfoBtn.Name = "saveInfoBtn";
+            this.saveInfoBtn.Size = new System.Drawing.Size(130, 52);
+            this.saveInfoBtn.TabIndex = 5;
+            this.saveInfoBtn.Text = "Save Info";
+            this.saveInfoBtn.UseVisualStyleBackColor = false;
+            // 
+            // FormUserData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.ClientSize = new System.Drawing.Size(798, 492);
+            this.Controls.Add(this.saveInfoBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.labelUserDataLast);
+            this.Controls.Add(this.labelUserDataName);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.iconUser);
+            this.Name = "FormUserData";
+            this.Text = "FormUserData";
+            this.Load += new System.EventHandler(this.FormUserData_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.iconUser)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FontAwesome.Sharp.IconPictureBox iconUser;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label labelUserDataName;
+        private System.Windows.Forms.Label labelUserDataLast;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button saveInfoBtn;
+    }
+}
---
+++ Stonks/Stonks.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace Stonks
+{
+    partial class Stonks
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Stonks));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.SettingsBtn = new FontAwesome.Sharp.IconButton();
+            this.UserDataBtn = new FontAwesome.Sharp.IconButton();
+            this.ProgressBtn = new FontAwesome.Sharp.IconButton();
+            this.iconButton3 = new FontAwesome.Sharp.IconButton();
+            this.PlanBtn = new FontAwesome.Sharp.IconButton();
+            this.DashboardBtn = new FontAwesome.Sharp.IconButton();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.TitleChildForm = new System.Windows.Forms.Label();
+            this.iconCurrentChildForm = new FontAwesome.Sharp.IconPictureBox();
+            this.panelShadow = new System.Windows.Forms.Panel();
+            this.panelDesktop = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelTitleBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconCurrentChildForm)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.panelMenu.Controls.Add(this.SettingsBtn);
+            this.panelMenu.Controls.Add(this.UserDataBtn);
+            this.panelMenu.Controls.Add(this.ProgressBtn);
+            this.panelMenu.Controls.Add(this.iconButton3);
+            this.panelMenu.Controls.Add(this.PlanBtn);
+            this.panelMenu.Controls.Add(this.DashboardBtn);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 640);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // SettingsBtn
+            // 
+            this.SettingsBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.SettingsBtn.FlatAppearance.BorderSize = 0;
+            this.SettingsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SettingsBtn.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.SettingsBtn.ForeColor = System.Drawing.Color.Gainsboro;
+            this.SettingsBtn.IconChar = FontAwesome.Sharp.IconChar.Cog;
+            this.SettingsBtn.IconColor = System.Drawing.Color.Gainsboro;
+            this.SettingsBtn.IconSize = 32;
+            this.SettingsBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.SettingsBtn.Location = new System.Drawing.Point(0, 440);
+            this.SettingsBtn.Name = "SettingsBtn";
+            this.SettingsBtn.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.SettingsBtn.Rotation = 0D;
+            this.SettingsBtn.Size = new System.Drawing.Size(220, 60);
+            this.SettingsBtn.TabIndex = 1;
+            this.SettingsBtn.Text = "Settings";
+            this.SettingsBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.SettingsBtn.UseVisualStyleBackColor = true;
+            this.SettingsBtn.Click += new System.EventHandler(this.SettingsBtn_Click);
+            // 
+            // UserDataBtn
+            // 
+            this.UserDataBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.UserDataBtn.FlatAppearance.BorderSize = 0;
+            this.UserDataBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UserDataBtn.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.UserDataBtn.ForeColor = System.Drawing.Color.Gainsboro;
+            this.UserDataBtn.IconChar = FontAwesome.Sharp.IconChar.Database;
+            this.UserDataBtn.IconColor = System.Drawing.Color.Gainsboro;
+            this.UserDataBtn.IconSize = 32;
+            this.UserDataBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.UserDataBtn.Location = new System.Drawing.Point(0, 380);
+            this.UserDataBtn.Name = "UserDataBtn";
+            this.UserDataBtn.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.UserDataBtn.Rotation = 0D;
+            this.UserDataBtn.Size = new System.Drawing.Size(220, 60);
+            this.UserDataBtn.TabIndex = 1;
+            this.UserDataBtn.Text = "My Data";
+            this.UserDataBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.UserDataBtn.UseVisualStyleBackColor = true;
+            this.UserDataBtn.Click += new System.EventHandler(this.UserDataBtn_Click);
+            // 
+            // ProgressBtn
+            // 
+            this.ProgressBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ProgressBtn.FlatAppearance.BorderSize = 0;
+            this.ProgressBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ProgressBtn.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.ProgressBtn.ForeColor = System.Drawing.Color.Gainsboro;
+            this.ProgressBtn.IconChar = FontAwesome.Sharp.IconChar.ChartLine;
+            this.ProgressBtn.IconColor = System.Drawing.Color.Gainsboro;
+            this.ProgressBtn.IconSize = 32;
+            this.ProgressBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ProgressBtn.Location = new System.Drawing.Point(0, 320);
+            this.ProgressBtn.Name = "ProgressBtn";
+            this.ProgressBtn.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.ProgressBtn.Rotation = 0D;
+            this.ProgressBtn.Size = new System.Drawing.Size(220, 60);
+            this.ProgressBtn.TabIndex = 1;
+            this.ProgressBtn.Text = "Progress";
+            this.ProgressBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.ProgressBtn.UseVisualStyleBackColor = true;
+            this.ProgressBtn.Click += new System.EventHandler(this.ProgressBtn_Click);
+            // 
+            // iconButton3
+            // 
+            this.iconButton3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.iconButton3.FlatAppearance.BorderSize = 0;
+            this.iconButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton3.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.iconButton3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.iconButton3.IconChar = FontAwesome.Sharp.IconChar.Wallet;
+            this.iconButton3.IconColor = System.Drawing.Color.Gainsboro;
+            this.iconButton3.IconSize = 32;
+            this.iconButton3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton3.Location = new System.Drawing.Point(0, 260);
+            this.iconButton3.Name = "iconButton3";
+            this.iconButton3.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.iconButton3.Rotation = 0D;
+            this.iconButton3.Size = new System.Drawing.Size(220, 60);
+            this.iconButton3.TabIndex = 1;
+            this.iconButton3.Text = "Your Financial Data";
+            this.iconButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.iconButton3.UseVisualStyleBackColor = true;
+            this.iconButton3.Click += new System.EventHandler(this.iconButton3_Click);
+            // 
+            // PlanBtn
+            // 
+            this.PlanBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PlanBtn.FlatAppearance.BorderSize = 0;
+            this.PlanBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PlanBtn.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.PlanBtn.ForeColor = System.Drawing.Color.Gainsboro;
+            this.PlanBtn.IconChar = FontAwesome.Sharp.IconChar.MoneyBillAlt;
+            this.PlanBtn.IconColor = System.Drawing.Color.Gainsboro;
+            this.PlanBtn.IconSize = 32;
+            this.PlanBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.PlanBtn.Location = new System.Drawing.Point(0, 200);
+            this.PlanBtn.Name = "PlanBtn";
+            this.PlanBtn.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.PlanBtn.Rotation = 0D;
+            this.PlanBtn.Size = new System.Drawing.Size(220, 60);
+            this.PlanBtn.TabIndex = 1;
+            this.PlanBtn.Text = "Savings Plan";
+            this.PlanBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.PlanBtn.UseVisualStyleBackColor = true;
+            this.PlanBtn.Click += new System.EventHandler(this.PlanBtn_Click);
+            // 
+            // DashboardBtn
+            // 
+            this.DashboardBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.DashboardBtn.FlatAppearance.BorderSize = 0;
+            this.DashboardBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DashboardBtn.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.DashboardBtn.ForeColor = System.Drawing.Color.Gainsboro;
+            this.DashboardBtn.IconChar = FontAwesome.Sharp.IconChar.ChartPie;
+            this.DashboardBtn.IconColor = System.Drawing.Color.Gainsboro;
+            this.DashboardBtn.IconSize = 32;
+            this.DashboardBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.DashboardBtn.Location = new System.Drawing.Point(0, 140);
+            this.DashboardBtn.Name = "DashboardBtn";
+            this.DashboardBtn.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.DashboardBtn.Rotation = 0D;
+            this.DashboardBtn.Size = new System.Drawing.Size(220, 60);
+            this.DashboardBtn.TabIndex = 1;
+            this.DashboardBtn.Text = "Dashboard";
+            this.DashboardBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.DashboardBtn.UseVisualStyleBackColor = true;
+            this.DashboardBtn.Click += new System.EventHandler(this.DashboardBtn_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Controls.Add(this.pictureBox1);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(220, 140);
+            this.panelLogo.TabIndex = 0;
+            this.panelLogo.Paint += new System.Windows.Forms.PaintEventHandler(this.panelLogo_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(220, 140);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(25)))), ((int)(((byte)(62)))));
+            this.panelTitleBar.Controls.Add(this.TitleChildForm);
+            this.panelTitleBar.Controls.Add(this.iconCurrentChildForm);
+            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar.Location = new System.Drawing.Point(220, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Size = new System.Drawing.Size(912, 69);
+            this.panelTitleBar.TabIndex = 1;
+            this.panelTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitleBar_MouseDown);
+            // 
+            // TitleChildForm
+            // 
+            this.TitleChildForm.AutoSize = true;
+            this.TitleChildForm.ForeColor = System.Drawing.Color.Gainsboro;
+            this.TitleChildForm.Location = new System.Drawing.Point(71, 23);
+            this.TitleChildForm.Name = "TitleChildForm";
+            this.TitleChildForm.Size = new System.Drawing.Size(50, 20);
+            this.TitleChildForm.TabIndex = 1;
+            this.TitleChildForm.Text = "Home";
+            // 
+            // iconCurrentChildForm
+            // 
+            this.iconCurrentChildForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(25)))), ((int)(((byte)(62)))));
+            this.iconCurrentChildForm.ForeColor = System.Drawing.Color.MediumPurple;
+            this.iconCurrentChildForm.IconChar = FontAwesome.Sharp.IconChar.Home;
+            this.iconCurrentChildForm.IconColor = System.Drawing.Color.MediumPurple;
+            this.iconCurrentChildForm.IconSize = 48;
+            this.iconCurrentChildForm.Location = new System.Drawing.Point(17, 12);
+            this.iconCurrentChildForm.Name = "iconCurrentChildForm";
+            this.iconCurrentChildForm.Size = new System.Drawing.Size(48, 49);
+            this.iconCurrentChildForm.TabIndex = 0;
+            this.iconCurrentChildForm.TabStop = false;
+            // 
+            // panelShadow
+            // 
+            this.panelShadow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(24)))), ((int)(((byte)(58)))));
+            this.panelShadow.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelShadow.Location = new System.Drawing.Point(220, 69);
+            this.panelShadow.Name = "panelShadow";
+            this.panelShadow.Size = new System.Drawing.Size(912, 8);
+            this.panelShadow.TabIndex = 2;
+            // 
+            // panelDesktop
+            // 
+            this.panelDesktop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.panelDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDesktop.Location = new System.Drawing.Point(220, 77);
+            this.panelDesktop.MinimumSize = new System.Drawing.Size(800, 500);
+            this.panelDesktop.Name = "panelDesktop";
+            this.panelDesktop.Size = new System.Drawing.Size(912, 563);
+            this.panelDesktop.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1132, 640);
+            this.Controls.Add(this.panelDesktop);
+            this.Controls.Add(this.panelShadow);
+            this.Controls.Add(this.panelTitleBar);
+            this.Controls.Add(this.panelMenu);
+            this.MaximumSize = new System.Drawing.Size(1300, 800);
+            this.MinimumSize = new System.Drawing.Size(970, 600);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelTitleBar.ResumeLayout(false);
+            this.panelTitleBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconCurrentChildForm)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelLogo;
+        private FontAwesome.Sharp.IconButton UserDataBtn;
+        private FontAwesome.Sharp.IconButton ProgressBtn;
+        private FontAwesome.Sharp.IconButton iconButton3;
+        private FontAwesome.Sharp.IconButton PlanBtn;
+        private FontAwesome.Sharp.IconButton DashboardBtn;
+        private FontAwesome.Sharp.IconButton SettingsBtn;
+        private System.Windows.Forms.Panel panelTitleBar;
+        private FontAwesome.Sharp.IconPictureBox iconCurrentChildForm;
+        private System.Windows.Forms.Label TitleChildForm;
+        private System.Windows.Forms.Panel panelShadow;
+        private System.Windows.Forms.Panel panelDesktop;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
