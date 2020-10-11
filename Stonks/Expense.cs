2996319e16d91b165673f27d10fa78c214bcf68b
Greta Zemgulyte <Greta.Zemgulyte@bentley.com>
2020-10-11 09:15:49 +0300

    Optimized and shortened code

--- Stonks/Expense.cs
+++ Stonks/Expense.cs
@@ -20,5 +20,12 @@ namespace Stonks
         public ExpenseType Type { get; set; }
         public double Value { get; set; }
         public double PlannedValue { get; set; }
+
+        public Expense (ExpenseType type, double value, double plannedValue)
+        {
+            Type = type;
+            Value = value;
+            PlannedValue = plannedValue;
+        }
     }
 }
--- Stonks/Forms/FormFinances.cs
+++ Stonks/Forms/FormFinances.cs
@@ -10,15 +10,34 @@ using System.Windows.Forms;
 
 namespace Stonks.Forms
 {
+
+    public static class DictionaryExt
+    {
+        public static Dictionary<ExpenseType, TextBox> AddBoxExpenseType(this Dictionary<ExpenseType, TextBox> source, ExpenseType key, TextBox value) 
+        {
+            source.Add(key, value);
+            return source;
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
+        {
+            foreach (T item in enumeration)
+            {
+                action(item);
+            }
+        }
+    }
+
     public partial class MyFinances : Form
     {
+        Dictionary<ExpenseType, TextBox> BoxExpenseType;
         
         public MyFinances()
         {   
             InitializeComponent();
             this.Text = "My Finances";
-            loadExpenses();
-            
+            InitBoxExpenseType();
+            LoadExpenses();
         }
 
         private void MyFinances_Load(object sender, EventArgs e)
@@ -26,27 +45,32 @@ namespace Stonks.Forms
 
         }
 
-        private void labelUtilities_Click(object sender, EventArgs e)
+        private void LabelUtilities_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void buttonSaveFinances_Click(object sender, EventArgs e)
+        private void ButtonSaveFinances_Click(object sender, EventArgs e)
         {
-            saveExpenseValues();
+            SaveExpenseValues();
             
         }
 
-        public void loadExpenses() {
-            var housingExpense = Program.financialPlan.GetExpense(ExpenseType.Housing);
-            var groceries = Program.financialPlan.GetExpense(ExpenseType.Groceries);
-            var transportExpense = Program.financialPlan.GetExpense(ExpenseType.Transport);
-            var entertainmentExpense = Program.financialPlan.GetExpense(ExpenseType.Entertainment);
-            var healthExpense = Program.financialPlan.GetExpense(ExpenseType.Health);
-            var shoppingExpense = Program.financialPlan.GetExpense(ExpenseType.Shopping);
-            var utilitiesExpense = Program.financialPlan.GetExpense(ExpenseType.Utilities);
-            var otherExpense = Program.financialPlan.GetExpense(ExpenseType.Other);
+        public void InitBoxExpenseType() {
+            BoxExpenseType = new Dictionary<ExpenseType, TextBox>();
+            BoxExpenseType
+                .AddBoxExpenseType(ExpenseType.Housing, textBoxHousing)
+                .AddBoxExpenseType(ExpenseType.Groceries, textBoxFood)
+                .AddBoxExpenseType(ExpenseType.Transport, textBoxTransport)
+                .AddBoxExpenseType(ExpenseType.Entertainment, textBoxEntertainment)
+                .AddBoxExpenseType(ExpenseType.Health, textBoxHealth)
+                .AddBoxExpenseType(ExpenseType.Shopping, textBoxShopping)
+                .AddBoxExpenseType(ExpenseType.Utilities, textBoxUtilities)
+                .AddBoxExpenseType(ExpenseType.Other, textBoxOther);
+        }
 
+        public void LoadExpenses() 
+        {
             //Income
             if (textBoxEarnings == null)
             {
@@ -57,91 +81,15 @@ namespace Stonks.Forms
                 textBoxEarnings.Text = Convert.ToString(Program.financialPlan.Income);
             }
 
-            //Housing
-            if (textBoxHousing == null || housingExpense == null)
-            {
-                textBoxHousing.Text = "0";
-            }
-            else
-            {
-                textBoxHousing.Text = Convert.ToString(housingExpense.Value);
-            }
-
-            //Groceries
-            if (textBoxFood == null || groceries == null)
-            {
-                textBoxFood.Text = "0";
-            }
-            else
-            {
-                textBoxFood.Text = Convert.ToString(groceries.Value);
-            }
-
-            //Transport
-            if (textBoxTransport == null || transportExpense == null) {
-                textBoxTransport.Text = "0";   
-            }
-            else
-            {
-                textBoxTransport.Text = Convert.ToString(transportExpense.Value);
-            }
-
-            //Entertainment
-            if (textBoxEntertainment == null || entertainmentExpense == null)
-            {
-                textBoxEntertainment.Text = "0";
-            }
-            else
-            {
-                textBoxEntertainment.Text = Convert.ToString(entertainmentExpense.Value);
-            }
-
-            //Health
-            if (textBoxHealth == null || healthExpense == null)
-            {
-                textBoxHealth.Text = "0";
-            }
-            else
-            {
-                textBoxHealth.Text = Convert.ToString(healthExpense.Value);
-            }
-
-            //Shopping
-            if (textBoxShopping == null || shoppingExpense == null)
-            {
-                textBoxShopping.Text = "0";
-            }
-            else
-            {
-                textBoxShopping.Text = Convert.ToString(shoppingExpense.Value);
-            }
-
-            //Utilities
-            if (textBoxUtilities == null || utilitiesExpense == null)
-            {
-                textBoxUtilities.Text = "0";
-            }
-            else
-            {
-                textBoxUtilities.Text = Convert.ToString(utilitiesExpense.Value);
-            }
-
-            //Other
-            if (textBoxOther == null || otherExpense == null)
-            {
-                textBoxOther.Text = "0";
-            }
-            else
-            {
-                textBoxOther.Text = Convert.ToString(otherExpense.Value);
-            }
+            //Expenses
+            BoxExpenseType.ForEach(x => x.Value.Text = (Program.financialPlan.GetExpense(x.Key) != null) ? Convert.ToString(Program.financialPlan.GetExpense(x.Key).Value) : "0");
 
             //Calculate Savings
             //FIX LATER
             //Program.financialPlan.Savings = Program.financialPlan.Income - (housingExpense.Value + groceries.Value + transportExpense.Value + entertainmentExpense.Value + healthExpense.Value + utilitiesExpense.Value + otherExpense.Value + shoppingExpense.Value);
         }
 
-        public void saveExpenseValues()
+        public void SaveExpenseValues()
         {
             //Income
             if (textBoxEarnings.Text != null)
@@ -149,83 +97,8 @@ namespace Stonks.Forms
                 Program.financialPlan.Income = Convert.ToDouble(textBoxEarnings.Text);
             }
 
-            //Housing
-            if (textBoxHousing.Text == null)
-            {
-                Program.financialPlan.AddExpense(new Expense() { Type = ExpenseType.Housing, PlannedValue = 0, Value = Convert.ToDouble(textBoxHousing.Text) });
-            }
-            else if (textBoxHousing.Text != null)
-            {
-                Program.financialPlan.ModifyExpense(new Expense() { Type = ExpenseType.Housing, PlannedValue = 0, Value = Convert.ToDouble(textBoxHousing.Text) });
-            }
-
-            //Groceries
-            if (textBoxFood.Text == null)
-            {
-                Program.financialPlan.AddExpense(new Expense() { Type = ExpenseType.Groceries, PlannedValue = 0, Value = Convert.ToDouble(textBoxFood.Text) });
-            }
-            else if (textBoxFood.Text != null)
-            {
-                Program.financialPlan.ModifyExpense(new Expense() { Type = ExpenseType.Groceries, PlannedValue = 0, Value = Convert.ToDouble(textBoxFood.Text) });
-            }
-
-            //Transport
-            if (textBoxTransport.Text == null)
-            {
-                Program.financialPlan.AddExpense(new Expense() { Type = ExpenseType.Transport, PlannedValue = 0, Value = Convert.ToDouble(textBoxTransport.Text) });
-            }
-            else if (textBoxTransport.Text != null)
-            {
-                Program.financialPlan.ModifyExpense(new Expense() { Type = ExpenseType.Transport, PlannedValue = 0, Value = Convert.ToDouble(textBoxTransport.Text) });
-            }
-
-            //Entertainment
-            if (textBoxEntertainment.Text == null)
-            {
-                Program.financialPlan.AddExpense(new Expense() { Type = ExpenseType.Entertainment, PlannedValue = 0, Value = Convert.ToDouble(textBoxEntertainment.Text) });
-            }
-            else if (textBoxTransport.Text != null)
-            {
-                Program.financialPlan.ModifyExpense(new Expense() { Type = ExpenseType.Entertainment, PlannedValue = 0, Value = Convert.ToDouble(textBoxEntertainment.Text) });
-            }
-
-            //Health
-            if (textBoxHealth.Text == null)
-            {
-                Program.financialPlan.AddExpense(new Expense() { Type = ExpenseType.Health, PlannedValue = 0, Value = Convert.ToDouble(textBoxHealth.Text) });
-            }
-            else if (textBoxHealth.Text != null)
-            {
-                Program.financialPlan.ModifyExpense(new Expense() { Type = ExpenseType.Health, PlannedValue = 0, Value = Convert.ToDouble(textBoxHealth.Text) });
-            }
-            //Shopping
-            if (textBoxShopping.Text == null)
-            {
-                Program.financialPlan.AddExpense(new Expense() { Type = ExpenseType.Shopping, PlannedValue = 0, Value = Convert.ToDouble(textBoxShopping.Text) });
-            }
-            else if (textBoxShopping.Text != null)
-            {
-                Program.financialPlan.ModifyExpense(new Expense() { Type = ExpenseType.Shopping, PlannedValue = 0, Value = Convert.ToDouble(textBoxShopping.Text) });
-            }
-            //Utilities
-            if (textBoxUtilities.Text == null)
-            {
-                Program.financialPlan.AddExpense(new Expense() { Type = ExpenseType.Utilities, PlannedValue = 0, Value = Convert.ToDouble(textBoxUtilities.Text) });
-            }
-            else if (textBoxUtilities.Text != null)
-            {
-                Program.financialPlan.ModifyExpense(new Expense() { Type = ExpenseType.Utilities, PlannedValue = 0, Value = Convert.ToDouble(textBoxUtilities.Text) });
-            }
-
-            //Other
-            if (textBoxOther.Text == null)
-            {
-                Program.financialPlan.AddExpense(new Expense() { Type = ExpenseType.Other, PlannedValue = 0, Value = Convert.ToDouble(textBoxOther.Text) });
-            }
-            else if (textBoxOther.Text != null)
-            {
-                Program.financialPlan.ModifyExpense(new Expense() { Type = ExpenseType.Other, PlannedValue = 0, Value = Convert.ToDouble(textBoxOther.Text) });
-            }
+            //Expenses
+            BoxExpenseType.ForEach(x => Program.financialPlan.AddExpense(new Expense(x.Key, Convert.ToDouble(x.Value.Text), 0)));
         }
     }
 }
